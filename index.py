cf8782f33bb59458e8db64e014b2cb66afb55e7c
davidasix <davidandersonsix@gmail.com>
2023-02-09 11:59:02 -0500

    Added timeout to orders page to ensure JS is loaded. Sorted through get_order_ids to output a list of orders

--- index.py
+++ index.py
@@ -1,6 +1,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 def login(d):
     # Get Credentials
@@ -18,25 +21,6 @@ def login(d):
     submit = d.find_element(By.XPATH, "//*[@type='submit'][contains(@value, 'sign-in')]")
     submit.click()
 
-
-def get_order_ids(d):
-    # Navigate to shop manager
-    shopManager = d.find_element(By.XPATH, "//a[label='Shop Manager']")
-    shopManager.click()
-
-    
-    orderLink = d.find_element(By.XPATH, "//a[contains(@href, 'orders/sold')]")
-    orderLink.click()
-
-    ordersComplete = d.find_element(By.XPATH, "//a[contains(@href, 'orders/sold/completed')]")
-    ordersComplete.click()
-
-    elements = d.find_elements(By.XPATH, "//input[contains(@href, '?order_id')]")
-
-    # Iterate through the elements and print their tag names
-    for element in elements:
-        print(element.tag_name)
-
 def add_cookies(d):
     cs = open('cookies', 'r')
     lines = 0
@@ -48,15 +32,35 @@ def add_cookies(d):
     print('Added', lines/2, 'cookies')
     return d
 
+def get_order_ids(d):
+    # elements = d.find_elements(By.XPATH, "//a[contains(@href, '?order_id')]")
+    orders = d.find_elements(By.XPATH, "//div[contains(@class, 'panel-body-row')]")
+    print('Found', len(orders), 'Orders')
+    # Iterate through the elements and print their tag names
+    print(orders)
+    for order in orders:
+        order_id = order.find_element(By.XPATH, "//a[contains(@href, '?order_id')]")
+        completed = order.find_element(By.XPATH, "div/div/div/div/div[@class='text-body-smaller']")
+        print(order_id.get_attribute('innerText'), completed.get_attribute('innerText'))
+
 def main():
     # Initialize the web browser
     driver = webdriver.Firefox()
     # Initially load a page to to avoid cookie aversion error
     driver.get('https://www.etsy.com')
+    driver.set_window_position(0, 0, windowHandle='current')
+    driver.set_window_size(900, 1000)
     # Load Auth cookies
     driver = add_cookies(driver)
     # Navigate to orders page
     driver.get("https://www.etsy.com/your/orders/sold/completed")
+    try:
+        print('Waiting for JS')
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//*[contains(@href, '?order_id')]")))
+        print("Page is ready!")
+    except TimeoutException:
+        print("Loading took too much time!")
+    get_order_ids(driver)
     # Close the web browser
     #driver.quit()
 
